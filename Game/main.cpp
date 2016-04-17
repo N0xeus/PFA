3099add3f96c24e78c7e7bb0022505d8ea65805f
Leo <leo.gar@hotmail.fr>
2016-04-17 19:40:20 +0200

    libs + includes

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    sf::RenderWindow window(sf::VideoMode(800,500), "Tesssssst!!!");
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Black);
+
+        sf::Sprite sprite;
+        sf::Texture texture;
+        if(!texture.loadFromFile("poke.jpg", sf::IntRect(125,100,50,50))){
+            cout << "erreur de chargelent de l'image" << endl;
+        }
+        texture.setSmooth(true);
+        sprite.setTexture(texture);
+        sprite.setColor(sf::Color(255,0,128));
+
+        sf::CircleShape shape(50);
+        shape.setFillColor(sf::Color(100,250,50));
+        shape.setPosition(75,75);
+
+
+        window.draw(sprite);
+        window.draw(shape);
+
+        window.display();
+    }
     return 0;
 }
