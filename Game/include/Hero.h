cd285fa3ad4c684079378b12a8a28613c37a3162
Leo <leo.gar@hotmail.fr>
2016-05-15 20:26:11 +0200

    re hero

--- Game/include/Hero.h
+++ Game/include/Hero.h
@@ -8,7 +8,7 @@ class Hero : public Character
 {
     public:
         Hero();
-        Hero(int id, const sf::FloatRect box, const sf::Vector2f speed, int hp, Attack& a, bool vulnerable, int lives, int state, float posX, float posY);
+        Hero(int id, const sf::FloatRect box, const sf::Vector2f speed, int hp, Attack& a, bool vulnerable, int lives, int state);
         virtual ~Hero();
 
         int Getlives() { return lives; }
--- Game/src/Hero.cpp
+++ Game/src/Hero.cpp
@@ -5,12 +5,10 @@ Hero::Hero()
     //ctor
 }
 
-Hero::Hero(int id, const sf::FloatRect box, const sf::Vector2f speed, int hp, Attack& a, bool vulnerable, int lives, int state, float posX, float posY):Character(id, box, speed, hp, a, vulnerable)
+Hero::Hero(int id, const sf::FloatRect box, const sf::Vector2f speed, int hp, Attack& a, bool vulnerable, int lives, int state):Character(id, box, speed, hp, a, vulnerable)
 {
     this->lives = lives;
     this->state = state;
-    this->posX = posX;
-    this->posY = posY;
 }
 
 Hero::~Hero()
